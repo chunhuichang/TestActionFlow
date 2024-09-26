717cdb7b0ef8394e9c513368aad596b4cbce9414
Jill Chang <jill20021@gmail.com>
2024-09-26 11:32:04 +0800

    Initial Commit

---
+++ TestActionFlow/TestActionFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestActionFlowApp.swift
+//  TestActionFlow
+//
+//  Created by Jill Chang on 2024/9/26.
+//
+
+import SwiftUI
+
+@main
+struct TestActionFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
